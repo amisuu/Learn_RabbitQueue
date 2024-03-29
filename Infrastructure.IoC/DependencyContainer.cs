59e4b46e39532c7fb80396232b0889c63dc86967
amisuu <liver13@wp.pl>
2024-03-29 14:35:36 +0100

    Added infractructer inversion of control component

---
+++ Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core.Bus;
+using Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
